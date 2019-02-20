a2a0c2a27bffd93976d0d48a98954c1074757949
Sandra <san.penno@gmail.com>
2019-02-20 17:38:01 +0000

    add files: app, landing, header and initial styling (logo, navbar, slideshows)

---
+++ app.js
@@ -0,0 +1,24 @@
+var express     = require("express"),
+    app         = express(),
+    path        = require("path"),
+    serveStatic = require('serve-static');
+    
+app.set("view engine", "ejs");
+app.use(serveStatic(path.join(__dirname, "public")));
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/about", function(req, res){
+   res.render("about");
+});
+
+app.get("/travel", function(req, res){
+   res.render("travel");
+});
+    
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server running"); 
+});
